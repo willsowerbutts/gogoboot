2f9ef9bc7c206b97dbd689f8683cd46e73e247b6
Will Sowerbutts <will@sowerbutts.com>
2023-07-30 11:37:07 +0100

    Revert "Revert "Optimised memset/memcpy/memmove from Linux""
    This reverts commit f88fc84499207c8366daefa755c9fbf9b06c6e67.

--- include/q40types.h
+++ include/q40types.h
@@ -1,24 +1,3 @@
-/* mytypes.h	*/
-/*
-	Copyright (C) 2011 John R. Coffman.
-	Licensed for hobbyist use on the N8VEM baby M68k CPU board.
-***********************************************************************
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    in the file COPYING in the distribution directory along with this
-    program.  If not, see <http://www.gnu.org/licenses/>.
-
-**********************************************************************/
 #ifndef __Q40TYPES_DOT_H__
 #define __Q40TYPES_DOT_H__
 
