ffb6d98475c248368b0bd6fae8a15d065aa9bd85
Will Sowerbutts <will@sowerbutts.com>
2023-08-10 09:56:44 +0100

    icmp: bug fix, always free packets even if we do not respond to them

--- icmp.c
+++ icmp.c
@@ -11,22 +11,24 @@ static timer_t icmp_throttle_timer = 0;
 
 static void icmp_send_unreachable(packet_sink_t *sink, packet_t *packet)
 {
-    if(!timer_expired(icmp_throttle_timer))
-        return;
+    // we use a timer to rate limit how fast we send ICMP unreachable messages
+    if(timer_expired(icmp_throttle_timer)){
+        icmp_throttle_timer = set_timer_ms(100);
 
-    // rate limit how fast we send ICMP unreachable messages
-    icmp_throttle_timer = set_timer_ms(100);
+        packet_t *unreach = packet_create_icmp(ntohl(packet->ipv4->source_ip), sizeof(ipv4_header_t) + 8);
+        packet_set_destination_mac(unreach, &packet->eth->source_mac);
 
-    packet_t *unreach = packet_create_icmp(ntohl(packet->ipv4->source_ip), sizeof(ipv4_header_t) + 8);
-    packet_set_destination_mac(unreach, &packet->eth->source_mac);
+        // fill in ICMP message
+        unreach->icmp->type = 3; // Destination Unreachable
+        unreach->icmp->code = 3; // Port Unreachable
+        memcpy(unreach->icmp->payload, packet->ipv4, sizeof(ipv4_header_t) + 8);
 
-    // fill in ICMP message
-    unreach->icmp->type = 3; // Destination Unreachable
-    unreach->icmp->code = 3; // Port Unreachable
-    memcpy(unreach->icmp->payload, packet->ipv4, sizeof(ipv4_header_t) + 8);
+        // transmit
+        net_tx(unreach);
+    }
 
+    // in all cases we have to free the incoming packet
     packet_free(packet);
-    net_tx(unreach);
 }
 
 static void icmp_received(packet_sink_t *sink, packet_t *packet)
@@ -47,7 +49,7 @@ static void icmp_received(packet_sink_t *sink, packet_t *packet)
         memcpy(packet->eth->source_mac, interface_macaddr, sizeof(macaddr_t));
 
         net_tx(packet);
-        // do not free received packet since it is now being re-used for transmission
+        // do NOT free received packet since it is now being re-used for transmission
     }else{
         // unhandled
         packet_free(packet);
